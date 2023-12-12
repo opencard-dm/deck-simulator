069e3d32e1f3faf86a0b6790518e5d4021c21ff3
takanari-saiki <stakanari987@gmail.com>
2023-12-12 23:26:58 +0900

    gcpのレジストリにイメージをプッシュするコマンドを追加

---
+++ tools/push.js
@@ -0,0 +1,25 @@
+const { spawn, execSync } = require('child_process')
+
+require('dotenv').config()
+if (!process.env.VERSION) {
+  throw new Error('process.env.VERSION is required')
+}
+
+const version = process.env.VERSION
+const repository = 'asia-northeast1-docker.pkg.dev/duel-masters-407617/tcg-simulator'
+
+// gcloud artifacts repositories list --location=asia-northeast1
+
+execSync(`docker tag tcg-simulator/app:${version} ${repository}/app:${version}`)
+
+spawn('docker', [
+  'push',
+  `${repository}/app:${version}`,
+]).stdout.on('data', (chunk) => {
+  process.stdout.write(chunk) // without new line
+}).on('close', () => {
+  console.log('docker push finished')
+  process.stdout.write(
+    execSync(`docker rmi ${repository}/app:${version}`)
+  )
+})
