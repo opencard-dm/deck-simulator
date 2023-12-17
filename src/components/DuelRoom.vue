4fb2c5274cddc5009402a2574a0a404f45cf33d9
takanari-saiki <stakanari987@gmail.com>
2023-12-17 20:35:55 +0900

    プレイヤーごとの領域を別ファイルに切り出し

--- src/components/DuelRoom.vue
+++ src/components/DuelRoom.vue
@@ -25,71 +25,17 @@
           height: playerZoneHeight,
         }">
           <template v-if="!single">
-            <TefudaZone
-              :side="'upper'"
+            <PlayerUpper
               :player="upperPlayer"
-              :tefudaCards="players[upperPlayer]['cards']['tefudaCards']"
-              v-on:move-cards="moveCards"
-            ></TefudaZone>
-            <ManaZone
-              :side="'upper'"
-              :player="upperPlayer"
-              :manaCards="players[upperPlayer]['cards']['manaCards']"
-              v-on:move-cards="moveCards"
-            ></ManaZone>
-            <PlayerZone
-              :side="'upper'"
-              :player="upperPlayer"
-              :bochiCards="players[upperPlayer]['cards']['bochiCards']"
-              :yamafudaCards="players[upperPlayer]['cards']['yamafudaCards']"
-              :shieldCards="players[upperPlayer]['cards']['shieldCards']"
-              :shieldCardGroups="
-                players[upperPlayer]['cards']['shieldCardGroups']
-              "
-              v-on:move-cards="moveCards"
-            >
-              <template #shield-zone>
-                <ShieldZone
-                  side="upper"
-                  :player="upperPlayer"
-                  :shieldCards="players[upperPlayer]['cards']['shieldCards']"
-                  :shieldCardGroups="
-                    players[upperPlayer]['cards']['shieldCardGroups']
-                  "
-                  v-on:move-cards="moveCards"
-                  @group-card="groupCard"
-                ></ShieldZone>
-              </template>
-              <template #deck-zone>
-                <DeckZone
-                  side="upper"
-                  :player="upperPlayer"
-                  :yamafudaCards="players[upperPlayer]['cards']['yamafudaCards']"
-                  v-on:move-cards="moveCards"
-                  @group-card="groupCard"
-                ></DeckZone>
-              </template>
-              <template #chojigenZone>
-                <ChojigenZone
-                  side="upper"
-                  :player="upperPlayer"
-                  :chojigenCards="players[upperPlayer]['cards']['chojigenCards']"
-                  :hasChojigen="players[upperPlayer].hasChojigen"
-                  @move-cards="moveCards"
-                ></ChojigenZone>
-              </template>
-            </PlayerZone>
-            <BattleZone
-              :side="'upper'"
-              :player="upperPlayer"
-              :battleCards="players[upperPlayer]['cards']['battleCards']"
-              :battleCardGroups="
-                players[upperPlayer]['cards']['battleCardGroups']
-              "
-              v-on:move-cards="moveCards"
+              :cards="players[upperPlayer].cards"
+              :name="players[upperPlayer].name"
+              :roomId="players[upperPlayer].roomId"
+              :isReady="players[upperPlayer].isReady"
+              :hasChojigen="players[upperPlayer].hasChojigen"
+              @move-cards="moveCards"
               @group-card="groupCard"
               @emit-room-state="emitRoomState"
-            ></BattleZone>
+            ></PlayerUpper>
           </template>
 
           <!-- <MessageBox :upper-player="upperPlayer"
@@ -98,76 +44,17 @@
 
           <!-- center -->
           <!-- <MessageButtons :player="lowerPlayer"></MessageButtons> -->
-
-          <BattleZone
-            :side="'lower'"
+          <PlayerLower
             :player="lowerPlayer"
-            :battleCards="players[lowerPlayer]['cards']['battleCards']"
-            :battleCardGroups="
-              players[lowerPlayer]['cards']['battleCardGroups']
-            "
-            v-on:move-cards="moveCards"
+            :cards="players[lowerPlayer].cards"
+            :name="players[lowerPlayer].name"
+            :roomId="players[lowerPlayer].roomId"
+            :isReady="players[lowerPlayer].isReady"
+            :hasChojigen="players[lowerPlayer].hasChojigen"
+            @move-cards="moveCards"
             @group-card="groupCard"
             @emit-room-state="emitRoomState"
-          ></BattleZone>
-
-          <player-zone
-            :side="'lower'"
-            :player="lowerPlayer"
-            :bochiCards="players[lowerPlayer]['cards']['bochiCards']"
-            :yamafudaCards="players[lowerPlayer]['cards']['yamafudaCards']"
-            :shieldCards="players[lowerPlayer]['cards']['shieldCards']"
-            :shieldCardGroups="
-              players[lowerPlayer]['cards']['shieldCardGroups']
-            "
-            v-on:move-cards="moveCards"
-          >
-            <template #shield-zone>
-              <ShieldZone
-                side="lower"
-                :player="lowerPlayer"
-                :shieldCards="players[lowerPlayer]['cards']['shieldCards']"
-                :shieldCardGroups="
-                  players[lowerPlayer]['cards']['shieldCardGroups']
-                "
-                v-on:move-cards="moveCards"
-                @group-card="groupCard"
-              ></ShieldZone>
-            </template>
-            <template #deck-zone>
-              <DeckZone
-                side="lower"
-                ref="lowerDeckZone"
-                :player="lowerPlayer"
-                :yamafudaCards="players[lowerPlayer]['cards']['yamafudaCards']"
-                v-on:move-cards="moveCards"
-                @group-card="groupCard"
-              ></DeckZone>
-            </template>
-            <template #chojigenZone>
-              <ChojigenZone
-                side="lower"
-                v-if="players[lowerPlayer]['cards']['chojigenCards'].length > 0"
-                :player="lowerPlayer"
-                :chojigenCards="players[lowerPlayer]['cards']['chojigenCards']"
-                :hasChojigen="players[lowerPlayer].hasChojigen"
-                @move-cards="moveCards"
-              ></ChojigenZone>
-            </template>
-          </player-zone>
-          <mana-zone
-            :side="'lower'"
-            :player="lowerPlayer"
-            :manaCards="players[lowerPlayer]['cards']['manaCards']"
-            v-on:move-cards="moveCards"
-          ></mana-zone>
-          <tefuda-zone
-            :side="'lower'"
-            :player="lowerPlayer"
-            :tefudaCards="players[lowerPlayer]['cards']['tefudaCards']"
-            v-on:move-cards="moveCards"
-            @drawOne="$refs.lowerDeckZone.drawOne()"
-          ></tefuda-zone>
+          ></PlayerLower>
         </div>
       </ImageViewer>
     </div>
@@ -178,6 +65,12 @@
 import { Layout } from '@/helpers/layout'
 import { isPhone } from '@/helpers/Util'
 import { onMounted, ref } from 'vue';
+import CHeader from "./CHeader.vue";
+import WorkSpace from "./WorkSpace.vue";
+import ImageViewer from "./ImageViewer.vue";
+import DeckSelector from "./DeckSelector.vue";
+import PlayerLower from './PlayerLower.vue';
+import PlayerUpper from './PlayerUpper.vue';
 
 const playerZoneHeight = isPhone() ? `${Layout.playerZoneHeight(70)}px` : false
 const isMounted = ref(false);
@@ -189,17 +82,6 @@ onMounted(() => {
 <script>
 import { Deck } from "@/helpers/Deck";
 import { Util } from "@/helpers/Util";
-import WorkSpace from "./WorkSpace.vue";
-import ImageViewer from "./ImageViewer.vue";
-import TefudaZone from "./TefudaZone.vue";
-import ManaZone from "./ManaZone.vue";
-import PlayerZone from "./PlayerZone.vue";
-import BattleZone from "./BattleZone.vue";
-import DeckSelector from "./DeckSelector.vue";
-import ShieldZone from "./ShieldZone.vue";
-import CHeader from "./CHeader.vue";
-import DeckZone from "./DeckZone.vue";
-import ChojigenZone from "./ChojigenZone.vue";
 import { SocketUtil } from '../helpers/socket';
 
 function initialData({ roomId }) {
@@ -247,21 +129,7 @@ function initialData({ roomId }) {
 }
 
 export default {
-  name: "c-app",
   props: ["upperPlayer", "lowerPlayer", "room", "loading", "deck", "single"],
-  components: {
-    WorkSpace,
-    ImageViewer,
-    TefudaZone,
-    ManaZone,
-    PlayerZone,
-    BattleZone,
-    DeckSelector,
-    ShieldZone,
-    CHeader,
-    DeckZone,
-    ChojigenZone,
-  },
   data() {
     const data = initialData({
       roomId: this.$route.query.roomId,
---
+++ src/components/PlayerLower.vue
@@ -0,0 +1,109 @@
+<template>
+  <BattleZone
+    :side="side"
+    :player="player"
+    :battleCards="cards.battleCards"
+    :battleCardGroups="cards.battleCardGroups"
+    @move-cards="moveCards"
+    @group-card="groupCard"
+    @emit-room-state="emitRoomState"
+  ></BattleZone>
+
+  <player-zone
+    :side="side"
+    :player="player"
+    :bochiCards="cards.bochiCards"
+    :yamafudaCards="cards.yamafudaCards"
+    :shieldCards="cards.shieldCards"
+    :shieldCardGroups="cards.shieldCardGroups"
+    @move-cards="moveCards"
+  >
+    <template #shield-zone>
+      <ShieldZone
+        :side="side"
+        :player="player"
+        :shieldCards="cards.shieldCards"
+        :shieldCardGroups="cards.shieldCardGroups"
+        @move-cards="moveCards"
+        @group-card="groupCard"
+      ></ShieldZone>
+    </template>
+    <template #deck-zone>
+      <DeckZone
+        :side="side"
+        ref="lowerDeckZone"
+        :player="player"
+        :yamafudaCards="cards.yamafudaCards"
+        @move-cards="moveCards"
+        @group-card="groupCard"
+      ></DeckZone>
+    </template>
+    <template #chojigenZone>
+      <ChojigenZone
+        :side="side"
+        v-if="cards.chojigenCards.length > 0"
+        :player="player"
+        :chojigenCards="cards.chojigenCards"
+        :hasChojigen="hasChojigen"
+        @move-cards="moveCards"
+      ></ChojigenZone>
+    </template>
+  </player-zone>
+  <mana-zone
+    :side="side"
+    :player="player"
+    :manaCards="cards.manaCards"
+    @move-cards="moveCards"
+  ></mana-zone>
+  <tefuda-zone
+    :side="side"
+    :player="player"
+    :tefudaCards="cards.tefudaCards"
+    @move-cards="moveCards"
+    @drawOne="$refs.lowerDeckZone.drawOne()"
+  ></tefuda-zone>
+</template>
+
+<script setup>
+import TefudaZone from './TefudaZone.vue';
+import ManaZone from './ManaZone.vue';
+import PlayerZone from './PlayerZone.vue';
+import BattleZone from './BattleZone.vue';
+import ShieldZone from './ShieldZone.vue';
+import DeckZone from './DeckZone.vue';
+import ChojigenZone from './ChojigenZone.vue';
+
+const props = defineProps({
+  player: String,
+  cards: {
+    manaCards: Array,
+    battleCards: Array,
+    bochiCards: Array,
+    shieldCards: Array,
+    tefudaCards: Array,
+    yamafudaCards: Array,
+    chojigenCards: Array,
+    // cardGroups
+    battleCardGroups: Array,
+    shieldCardGroups: Array,
+  },
+  name: String,
+  roomId: String,
+  isReady: Boolean,
+  hasChojigen: Boolean,
+});
+
+const side = 'lower';
+
+const emit = defineEmits(['move-cards', 'group-card', 'emit-room-state']);
+
+function moveCards(from, to, cards, player, prepend) {
+  emit('move-cards', ...arguments);
+}
+function groupCard(from, to, fromCard, toCard, player) {
+  emit('group-card', ...arguments);
+}
+function emitRoomState() {
+  emit('emit-room-state', props.player);
+}
+</script>
