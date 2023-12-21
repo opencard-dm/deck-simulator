312431f48520fedec5b9ed42f5ee9ba43ced1e60
saiki <takanari-saiki@upload-gp.co.jp>
2023-12-21 17:22:58 +0900

    vitestを設定

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,26 @@
 // vite.config.js
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { defineConfig } from 'vite'
 import markdownRawPlugin from 'vite-raw-plugin'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default {
+export default defineConfig({
     plugins: [
         vue(),
         markdownRawPlugin({
             fileRegex: /\.md$/
-        })
+        }),
+        tsconfigPaths(),
     ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src')
         },
-    }
-}
+    },
+    test: {
+        globals: true,
+        environment: 'happy-dom',
+        setupFiles: './tests/vitest.setup.ts',
+    },
+})
