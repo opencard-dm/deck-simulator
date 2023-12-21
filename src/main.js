7e307bb9df9cc9678a46524de76a133e6cbabc84
saiki <takanari-saiki@upload-gp.co.jp>
2023-12-21 13:33:04 +0900

    不要な記述を削除

--- src/main.js
+++ src/main.js
@@ -22,9 +22,6 @@ vueApp.use(useConfig)
 //
 // axios
 import axios from 'axios'
-if (import.meta.env.VUE_APP_WS_HOST) {
-  axios.defaults.baseURL = import.meta.env.VUE_APP_WS_HOST
-}
 
 //
 // oruga ui
