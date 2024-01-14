fd6f41eb947f504d7a379829755e63a8cc4035cf
takanari-saiki <stakanari987@gmail.com>
2024-01-14 20:36:33 +0900

    型CardDetailを追加

--- src/entities/Deck.ts
+++ src/entities/Deck.ts
@@ -1,12 +1,39 @@
 import { Card } from "./Card";
 
+export interface CardDetail {
+    main_card_id: number
+    name: string
+    name_ruby: string
+    image_paths: string[]
+    sub_types: {
+        sub_type_id: number
+        name: string
+        sort: number
+    }[]
+    is_light: boolean
+    is_water: boolean
+    is_dark: boolean
+    is_fire: boolean
+    is_nature: boolean
+    is_zero: boolean
+    power: string
+    power_int: number
+    cost: number
+    races: {
+      race_id: number
+      name: "ビーストフォーク"
+      sort: number
+    }[]
+    card_text: string
+}
+
 export interface Deck {
     name: string
     dmDeckId: string
     cards: Card[]
     chojigenCards: Card[]
     grCards: Card[]
-    cardDetails?: any
+    cardDetails?: {[key: string]: CardDetail}
 }
 
 export type GmCard = {
