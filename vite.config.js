de5ec98edb7178bf7dafc35ef24bfb5f994ff5ed
takanari-saiki <stakanari987@gmail.com>
2024-01-21 15:28:33 +0900

    Google Analyticsのタグをライブラリ管理に変更

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 // vite.config.js
+import 'dotenv/config'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
 import markdownRawPlugin from 'vite-raw-plugin'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import 'dotenv/config'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 export default defineConfig(({ mode }) => {
     return {
@@ -14,6 +15,11 @@ export default defineConfig(({ mode }) => {
                 fileRegex: /\.md$/
             }),
             tsconfigPaths(),
+            VitePluginRadar({
+                analytics: {
+                    id: 'G-MC3V0FB8RH',
+                }
+            })
         ],
         resolve: {
             alias: {
