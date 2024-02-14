e8166d51e57b0ab57a737348080c62a0d56c11a5
takanari-saiki <stakanari987@gmail.com>
2024-02-14 09:07:35 +0900

    デッキデータを新しい形式に置き換え

--- tools/getDecksFromAirtable.ts
+++ tools/getDecksFromAirtable.ts
@@ -1,14 +1,11 @@
 /**
- * 現在airtableはローカルでのデッキデータのjsonファイル作成に使用し、本番環境では使用していません。
+ * カード名からAirtableのIDを検索し付与します。
  */
 import 'dotenv/config'
-import { CardDetail, SourceDeck } from '@/entities/Deck';
+import { SourceDeck } from '@/entities/Deck';
 import Airtable from 'airtable'
-import { Record, FieldSet } from 'airtable'
 import { readFileSync,  writeFileSync } from 'fs'
 import decks from '../src/decks.json' assert { type: "json" }
-
-type CardDetails = {[key: string]: CardDetail}
 
 console.assert(process.env.AIRTABLE_SECRET_TOKEN, 'AIRTABLE_SECRET_TOKEN is required')
 
@@ -17,32 +14,29 @@ var base = new Airtable({
 }).base('appNBBdv4EODRJJJI');
 
 const deckFiles = [
-    // 'tools/json/202401_赤青マジック.json',
-    // 'tools/json/202401_アナカラージャオウガ.json',
-    // 'tools/json/202401_黒緑アビス.json',
-    // 'tools/json/202402_フィオナアカシック.json',
-    // 'tools/json/202402_青黒魔道具.json',
+    'tools/json/202401_赤青マジック.json',
+    'tools/json/202402_フィオナアカシック.json',
+    'tools/json/202401_アナカラージャオウガ.json',
+    'tools/json/202401_黒緑アビス.json',
+    'tools/json/202402_青黒魔道具.json',
     'tools/json/202402_赤緑アポロ.json',
     'tools/json/202402_青黒コンプレックス_超CSⅥ福岡4位.json',
 ]
 
 for (const file of deckFiles) {
-    const cardDetails: CardDetails = {}
     const sourceDeck: SourceDeck = JSON.parse(readFileSync(file, 'utf8'))
 
     for (const card of sourceDeck.cards) {
-        await fetchCardByName(card.name, cardDetails)
+        card.cd = await fetchCardIdByName(card.name)
     }
     sourceDeck.source = 'airtable'
-    sourceDeck.cardDetails = cardDetails
     // デッキを追加
     decks.push(sourceDeck as any)
     console.log(sourceDeck)
 }
 writeFileSync('./src/decks.json', JSON.stringify(decks, null, 2));
 
-async function fetchCardByName(name: string, cardDetails: CardDetails) {
-
+async function fetchCardIdByName(name: string) {
     const records = await base('cards').select({
         // Selecting the first 3 records in Grid view:
         maxRecords: 3,
@@ -52,39 +46,9 @@ async function fetchCardByName(name: string, cardDetails: CardDetails) {
     if (records.length === 0) {
         console.error(`カード"${name}"が見つかりませんでした`)
     }
+    if (records.length >= 2) {
+        console.warn('重複', name)
+    }
     const record = records[0]
-    cardDetails[name] = getCardDetailFromRecord(record)
-    if (record.get('combined_card')) {
-        const combinedCard = await base('cards').find(record.get('combined_card') as string)
-        cardDetails[name].combined_card = getCardDetailFromRecord(combinedCard)
-    }
-}
-
-function getCardDetailFromRecord(record: Record<FieldSet>): CardDetail {
-    return {
-        // カード名をIDにする
-        id: record.get('name') as string,
-        name: record.get('name') as string,
-        cost: record.get('cost') as number,
-        power: record.get('power') as string,
-        card_text: record.get('text') as string,
-        civilizations: Array.from(record.get('civilizations') as any).map((c) => {
-            switch (c) {
-                case '光':
-                    return 'light';
-                case '水':
-                    return 'water';
-                case '闇':
-                    return 'dark';
-                case '火':
-                    return 'fire';
-                case '自然':
-                    return 'nature';
-                case 'ゼロ':
-                    return 'zero';
-                default:
-                    c;
-            }
-        }) as string[],
-    }
+    return record.id
 }
