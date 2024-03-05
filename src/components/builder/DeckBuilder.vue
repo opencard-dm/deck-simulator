0a8ce2dc4af4df11fd0854a158cf2d4bb06b780c
takanari-saiki <stakanari987@gmail.com>
2024-03-05 11:18:54 +0900

    Composition APIにリファクタリング

--- src/components/builder/DeckBuilder.vue
+++ src/components/builder/DeckBuilder.vue
@@ -28,7 +28,45 @@
 
       <div class="content">
         <div class="deck-wrapper left">
-          <div id="deck1">
+          <div>
+            <OField
+              class="deckInput"
+              :style="{
+                paddingTop: '6px',
+                paddingLeft: '8px',
+              }"
+              :variant="error ? 'danger' : ''"
+              :message="loading ? 'カードが見つかりませんでした' : error"
+            >
+              <OInput
+                list="card_left"
+                v-model="cardname"
+                :style="{
+                  paddingRight: '0px',
+                  minWidth: '200px',
+                }"
+                size="small"
+                placeholder="カード名を入力"
+                :expanded="false"
+              >
+              </OInput>
+              <!-- <input  name="browser"> -->
+              <o-button variant="info" 
+                @click=""
+                size="small"
+                :disabled="false"
+                >追加</o-button
+              >
+              <datalist id="card_left">
+                <option 
+                  v-for="cardname in Object.keys(cardnames)"
+                  :key="cardname" 
+                  :value="cardname"
+                ></option>
+              </datalist>
+            </OField>
+          </div>
+          <div>
             <CardList
               :cards="left.deckData.cards"
               :deck="left.deckData"
@@ -39,7 +77,7 @@
         </div>
 
         <div class="deck-wrapper right">
-          <div id="deck2">
+          <div>
             <CardList
               :cards="right.deckData.cards"
               :deck="right.deckData"
@@ -68,154 +106,162 @@
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
 import DeckHeader from "./DeckHeader.vue";
 import CardList from "./CardList.vue";
-</script>
-<script lang="ts">
-import { Deck } from "@/helpers/Deck";
+import { ModelSelect } from 'vue-search-select'
+import { Deck, fetchDeck } from "@/helpers/Deck";
 import systemDecks from '@/decks.json'
+import { ref, reactive, computed } from 'vue'
+import { useRoomStore } from "@/stores/room";
+import { SourceDeck } from "@/entities/Deck";
+import { useDecksStore } from "@/stores/decks";
+import cardnames from '@/cardnames.json'
+import { isPhone } from "@/helpers/Util";
 
-export default {
-  data() {
-    return {
-      // apiで取得
-      deckList: {
-        readyMade: [],
-        custom: [],
-      },
-      left: {
-        deckIndex: 0,
-        deckData: {
-          cards: [],
-        },
-      },
-      right: {
-        deckIndex: 1,
-        deckData: {
-          cards: [],
-        },
-      },
-      display: {
-        card: null,
-        left: true,
-        hidden: false,
-        blur: false,
-        imageUrl: "",
-      },
-      preview: {
-        left: {
-          src: "",
-          input: null,
-          file: null,
-          files: null,
-        },
-        right: {
-          src: "",
-          input: null,
-          file: null,
-          files: null,
-        },
-      },
-      message: "",
-    };
+// data
+const deckList = reactive({
+  readyMade: [] as SourceDeck[],
+  custom: [] as SourceDeck[],
+})
+const cardname = ref('')
+const left = reactive({
+  deckIndex: 0,
+  deckData: {
+    cards: [],
   },
-  created() {
-    this.message = "データを\n取得中です";
-    const decks = [];
-    this.$store.state.decks.data.forEach(source => {
-      decks.push(...source.decks)
-    })
-    decks.push(...systemDecks)
-    this.deckList.custom = decks;
-    if (decks[0]) {
-      this.left.deckData = Deck.formatData(decks[0]);
-    }
-    if (decks[1]) {
-      this.right.deckData = Deck.formatData(decks[1]);
-    }
-    this.message = "";
+})
+const right = reactive({
+  deckIndex: 1,
+  deckData: {
+    cards: [],
   },
-  methods: {
-    updateDeck(params, side) {
-      this.message = "変更を\n保存中です";
-      // 名前を変更
-      if (params.name) {
-        this[side].deckData.name = params.name;
-      }
-      // カードを追加
-      if (params.cardUrl) {
-        this[side].deckData.cards.push({
-          imageUrl: params.cardUrl,
-          time: 0,
-        });
-      }
-      const decksCopy = this.$store.state.decks.data;
-      decksCopy[this[side].deckIndex] = this[side].deckData;
-      this.$store.commit("decks/setData", decksCopy);
-      this.message = "";
-    },
-    createDeck(params, side) {
-      if (!params.name) return;
-      const deck = {
-        name: params.name,
-        cards: [],
-      };
-      const decksCopy = this.$store.state.decks.data;
-      decksCopy.push(deck);
-      this.$store.commit("decks/setData", decksCopy);
-      this[side]["deckData"] = deck;
-    },
-    changeDeck(deckType, index, side) {
-      if (deckType === "custom") {
-        this[side]["deckData"] = Deck.formatData(
-          this.deckList["custom"][index],
-          this.useConfig().IMAGE_HOST
-        );
-        this[side].deckIndex = index;
-      }
-    },
-    deleteDeck(side) {
-      const decksCopy = this.$store.state.decks.data;
-      decksCopy.splice(this[side].deckIndex, 1);
-      this.$store.commit("decks/setData", decksCopy);
-      this.message = "";
-      location.reload();
-    },
-    totalNum(cards) {
-      let result = 0;
-      for (let card of cards) {
-        result += card.time;
-      }
-      return result;
-    },
-    onDragOver() {
-      event.preventDefault();
-      this.display.imageUrl = null;
-    },
-    onDragStart() {
-      // event.preventDefault()
-    },
-    traceMouseMove(event) {
-      if (this.display.hidden) {
-        return;
-      }
-      const imageSrc = event.target.src;
-      if (!imageSrc) {
-        this.display.imageUrl = "";
-        return;
-      }
-      this.display.imageUrl = imageSrc;
-      let mX = event.pageX;
-      // let mY = event.pageY;
-      if (mX < window.innerWidth / 2) {
-        this.display.left = false;
-      } else {
-        this.display.left = true;
-      }
-    },
+})
+const display = reactive({
+  card: null,
+  left: true,
+  hidden: false,
+  blur: false,
+  imageUrl: "",
+})
+const preview = reactive({
+  left: {
+    src: "",
+    input: null,
+    file: null,
+    files: null,
   },
-};
+  right: {
+    src: "",
+    input: null,
+    file: null,
+    files: null,
+  },
+})
+const message = ref('')
+
+const roomStore = useRoomStore()
+const decksStore = useDecksStore();
+
+// on created
+(function () {
+  message.value = "データを\n取得中です";
+  const decks: SourceDeck[] = [];
+  decksStore.data.forEach(source => {
+    decks.push(...source.decks)
+  })
+  decks.push(...systemDecks as any[])
+  deckList.custom = decks;
+  if (decks[0]) {
+    left.deckData = Deck.formatData(decks[0]);
+    fetchDeck(decks[0].name, roomStore)
+  }
+  if (decks[1]) {
+    right.deckData = Deck.formatData(decks[1]);
+    fetchDeck(decks[1].name, roomStore)
+  }
+  message.value = "";
+})();
+
+// methods
+function updateDeck(params, side) {
+  message.value = "変更を\n保存中です";
+  // 名前を変更
+  if (params.name) {
+    this[side].deckData.name = params.name;
+  }
+  // カードを追加
+  if (params.cardUrl) {
+    this[side].deckData.cards.push({
+      imageUrl: params.cardUrl,
+      time: 0,
+    });
+  }
+  const decksCopy = this.$store.state.decks.data;
+  decksCopy[this[side].deckIndex] = this[side].deckData;
+  this.$store.commit("decks/setData", decksCopy);
+  message.value = "";
+}
+function createDeck(params, side) {
+  if (!params.name) return;
+  const deck = {
+    name: params.name,
+    cards: [],
+  };
+  const decksCopy = this.$store.state.decks.data;
+  decksCopy.push(deck);
+  this.$store.commit("decks/setData", decksCopy);
+  this[side]["deckData"] = deck;
+}
+function changeDeck(deckType, index, side) {
+  if (deckType === "custom") {
+    this[side]["deckData"] = Deck.formatData(
+      this.deckList["custom"][index],
+      this.useConfig().IMAGE_HOST
+    );
+    this[side].deckIndex = index;
+  }
+}
+function deleteDeck(side) {
+  const decksCopy = this.$store.state.decks.data;
+  decksCopy.splice(this[side].deckIndex, 1);
+  this.$store.commit("decks/setData", decksCopy);
+  this.message = "";
+  location.reload();
+}
+function totalNum(cards) {
+  let result = 0;
+  for (let card of cards) {
+    result += card.time;
+  }
+  return result;
+}
+function onDragOver() {
+  event.preventDefault();
+  this.display.imageUrl = null;
+}
+function onDragStart() {
+  // event.preventDefault()
+}
+function traceMouseMove(event) {
+  if (display.hidden) {
+    return;
+  }
+  const imageSrc = event.target.src;
+  if (!imageSrc) {
+    display.imageUrl = "";
+    return;
+  }
+  display.imageUrl = imageSrc;
+  let mX = event.pageX;
+  // let mY = event.pageY;
+  if (mX < window.innerWidth / 2) {
+    display.left = false;
+  } else {
+    display.left = true;
+  }
+}
 </script>
 
 <style lang="scss" scoped>
